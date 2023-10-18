1588aea2b1707a0ba7674c28122ecaa60089f667
EgorCharntsev <charncev200447@gmail.com>
2023-10-18 03:58:29 +0300

    init commit

---
+++ src/main/java/ru/kpfu/itis/charntsev/net/servlet/LoginServlet.java
@@ -0,0 +1,21 @@
+package ru.kpfu.itis.charntsev.net.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "loginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("login.html");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
